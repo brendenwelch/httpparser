2077ee339f0b1eb7654771219789ddd7e8850338
Brenden Welch <aloofd@gmail.com>
2025-08-17 16:26:33 -0700

    Reads data from file 8 bytes at a time, constructs 'lines' until newline

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("failed to open file:", err)
+	}
+
+	var line []byte
+	for {
+		data := make([]byte, 8)
+		_, err := file.Read(data)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal("failed to read data:", err)
+		}
+
+		parts := bytes.Split(data, []byte("\n"))
+		for idx := range parts {
+			line = append(line, parts[idx]...)
+			if len(parts[idx:]) > 1 {
+				fmt.Println("read:", string(line))
+				line = nil
+			}
+		}
+	}
 }
