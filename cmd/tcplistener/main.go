091c418842f7b79ba30cac7101ac5f5765dd1c07
Brenden Welch <aloofd@gmail.com>
2025-08-17 23:27:27 -0700

    Connected request line parser to tcp listener. Currently hangs until request connection closes

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,62 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"os"
+
+	"github.com/brendenwelch/httpparser/internal/request"
 )
 
-func getLinesChannel(stream io.ReadCloser) <-chan string {
-	out := make(chan string)
-
-	go func() {
-		defer stream.Close()
-		defer close(out)
-
-		var line []byte
-		for {
-			data := make([]byte, 8)
-			_, err := stream.Read(data)
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				log.Fatal("failed to read data: ", err)
-			}
-
-			parts := bytes.Split(data, []byte("\n"))
-			for idx := range parts {
-				line = append(line, parts[idx]...)
-				if len(parts[idx:]) > 1 {
-					out <- string(line)
-					line = nil
-				}
-			}
-		}
-		if len(line) != 0 {
-			out <- string(line)
-		}
-	}()
-
-	return out
-}
-
-func ReadFromFile(filepath string) {
-	file, err := os.Open(filepath)
-	if err != nil {
-		log.Fatal("failed to open file: ", err)
-	}
-
-	lines := getLinesChannel(file)
-	for line := range lines {
-		fmt.Println(line)
-	}
-}
-
-func ReadFromTCP(address string) {
-	listener, err := net.Listen("tcp", address)
+func main() {
+	listener, err := net.Listen("tcp", "localhost:42069")
 	if err != nil {
 		log.Fatal("failed to open tcp listener: ", err)
 	}
@@ -68,15 +21,14 @@ func ReadFromTCP(address string) {
 			log.Fatal("failed to accept connection from listener: ", err)
 		}
 
-		fmt.Println("Connection accepted")
-		lines := getLinesChannel(conn)
-		for line := range lines {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("failed to process request from reader: ", err)
 		}
-		fmt.Println("Connection closed")
-	}
-}
 
-func main() {
-	ReadFromTCP("localhost:42069")
+		fmt.Println("Request line:")
+		fmt.Println("- Method:", req.RequestLine.Method)
+		fmt.Println("- Target:", req.RequestLine.RequestTarget)
+		fmt.Println("- Version:", req.RequestLine.HttpVersion)
+	}
 }
